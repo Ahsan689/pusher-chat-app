af5042709e82627673cb6e86455907ac757fa377
unknown <ahsanaleem689@gmail.com>
2022-10-20 23:44:16 +0500

    new commits

--- pages/chat.js
+++ pages/chat.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState , useRef } from "react";
 import Pusher from "pusher-js";
 import SendMessage from "../components/SendMessage";
 import axios from "axios";
@@ -20,43 +20,26 @@ const Chat = ({ username, userLocation }) => {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [usersRemoved, setUsersRemoved] = useState([]);
 
-  const pusher = new Pusher(process.env.NEXT_PUBLIC_KEY, {
-    cluster: 'ap1',
-    // use jwts in prod
-    // httpsHost: 'https://pusher-chat-app-mu.vercel.app',
-    userAuthentication:{
-      endpoint: 'api/pusher/auth',
-      transport: "jsonp",
-      auth: {
-          params: {username},
-      },
-
-    },
-    // transport: "ajax",
-    // authEndpoint: `api/pusher/auth`,
-    // auth: { params: {username}},
-    // use webSocket only
-  });
-
+  let pusher = useRef(null) ;
 
   useEffect(() => {
 
-    const channel = pusher.subscribe("presence-channel"); 
-    console.log(channel,'chennallll');
+    pusher.current = new Pusher(process.env.NEXT_PUBLIC_KEY, {
+      cluster: 'ap1',
+      // use jwts in prod
 
-    // async function pushData(data) {
-    //   const res = await fetch('/api/pusher/auth', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({auth: { params: {username}}}),
-    //   });
-    //   if (!res.ok) {
-    //     console.error('failed to push data');
-    //   }
-    // }
-    // pushData()
+      userAuthentication:{
+        endpoint: 'api/pusher/auth',
+        // auth: { params: {username}},
+  
+      },
+      // transport: "ajax",
+      // authEndpoint: `api/pusher/auth`,
+      // use webSocket only
+    });
+
+    const channel = pusher.current.subscribe("presence-channel"); 
+    console.log(channel,'chennallll');
 
 
 
@@ -101,12 +84,12 @@ const Chat = ({ username, userLocation }) => {
     });
 
     return () => {
-      pusher.unsubscribe("presence-channel");
+      pusher.current.unsubscribe("presence-channel");
     };
   }, []);
 
   const handleSignOut = () => {
-    pusher.unsubscribe("presence-channel");
+    pusher.current.unsubscribe("presence-channel");
     router.push("/");
   };
 
