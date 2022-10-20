78cfcd89991cec741d94d08fb98ceaa5c7f47be1
unknown <ahsanaleem689@gmail.com>
2022-10-20 15:47:57 +0500

    new commit

--- pages/api/pusher/auth/index.js
+++ pages/api/pusher/auth/index.js
@@ -2,7 +2,7 @@ import {pusher} from '../../../../lib/pusher'
 
 export default async function handler( req, res ) {
   // see https://pusher.com/docs/channels/server_api/authenticating-users
-  const { socket_id, channel_name, username, userLocation } = req.body;
+  const { socket_id, channel_name, username } = req.body;
 
   // use JWTs here to authenticate users before continuing
 
@@ -12,7 +12,7 @@ export default async function handler( req, res ) {
     user_id: randomString,
     user_info: {
       username: "@" + username,
-      userLocation
+    //   userLocation
     },
   };
 
