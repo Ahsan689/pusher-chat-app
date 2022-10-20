c030adb4e340daf86d6c7eb113e89bde42c5c9ea
unknown <ahsanaleem689@gmail.com>
2022-10-20 23:53:53 +0500

    new commits

--- pages/api/pusher/auth/index.js
+++ pages/api/pusher/auth/index.js
@@ -1,6 +1,31 @@
-import {pusher} from '../../../../lib/pusher'
+import {pusher} from '../../../../lib/pusher.js'
 
-export const handler =  async ( 'pusher/auth'  ,( req, res ) => {
+// export default async function handler(  req, res ) {
+//   // see https://pusher.com/docs/channels/server_api/authenticating-users
+//   const { socket_id, channel_name, username } = req.body;
+
+//   // use JWTs here to authenticate users before continuing
+
+//   const randomString = Math.random().toString(36).slice(2);
+
+//   const presenceData = {
+//     user_id: randomString,
+//     user_info: {
+//       username: "@" + username,
+//     //   userLocation
+//     },
+//   };
+
+//   try {
+//     const auth = pusher.authenticate(socket_id, channel_name, presenceData);
+//     res.send(auth);    
+//   } catch (error) {
+//       console.error(error)
+//   }
+  
+// }
+
+module.exports = ( req, res ) => {
   // see https://pusher.com/docs/channels/server_api/authenticating-users
   const { socket_id, channel_name, username } = req.body;
 
@@ -23,5 +48,5 @@ export const handler =  async ( 'pusher/auth'  ,( req, res ) => {
       console.error(error)
   }
   
-})
+}
 
--- pages/api/pusher/chat-update.js
+++ pages/api/pusher/chat-update.js
@@ -1,7 +1,21 @@
 import { pusher } from "../../../lib/pusher";
 
 // presence channel handler
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
+//   const { message, username, userLocation } = req.body;
+
+//   // console.log(message,"messageeeee");
+//   // trigger a new post event via pusher
+//   await pusher.trigger("presence-channel", "chat-update", {
+//     message,
+//     username,
+//     userLocation
+//   });
+
+//   res.json({ status: 200 });
+// }
+
+module.exports = async (req, res) => {
   const { message, username, userLocation } = req.body;
 
   // console.log(message,"messageeeee");
